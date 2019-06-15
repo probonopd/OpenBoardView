28c544ff36adab59d050f61ae4e4f0073b83ef80
probonopd <probonopd@users.noreply.github.com>
2019-06-15 19:47:16 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -97,7 +97,7 @@ if [ "$DEPLOY_APPIMAGE" == "yes" ] ; then
   find "$DESTDIR"
   wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
   chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-  ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+  ./linuxdeployqt-continuous-x86_64.AppImage "$DESTDIR"/usr/share/applications/*.desktop -appimage
   wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
   bash upload.sh Open*.AppImage*
 fi
